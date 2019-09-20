00ad980af6a6cec49d8a155751faf568a220040d
abhishek.singh <abhishek.singh@digispice.com>
2019-09-20 13:26:25 +0530

    logger added Dockerfile updated

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM openjdk:8-jdk-alpine
 VOLUME /tmp
+VOLUME /logs/dockerSpringBoot
 #ARG JAR_FILE
 COPY target/app.jar app.jar
 ENTRYPOINT ["java","-jar","app.jar"]
--- src/main/java/com/abhi/DockerSpringBootApplication.java
+++ src/main/java/com/abhi/DockerSpringBootApplication.java
@@ -1,8 +1,9 @@
 package com.abhi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DockerSpringBootApplication {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@RequestMapping(value="/v1/test",method = RequestMethod.GET)
 	public String dockerTesting() {
+		logger.info("Testing logger in spring boot docker application.");
 		return "Hello world";
 	}
 	
